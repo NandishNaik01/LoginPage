d8c36a78a8c813b7f23867c88d32f4ba2e149342
NandishNaik01 <naik.nandishd@gmail.com>
2024-01-06 23:00:38 +0530

    this is rendering commit

---
+++ authenication/main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.static_folder = 'static'
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/signup")
+def signup():
+    return render_template('signup.html')
+
+@app.route("/forgot")
+def forgot():
+    return render_template('forgotpass.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
